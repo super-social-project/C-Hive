6205175b636bfb9fbdcceadfb5f2f1a3d5d8a9fb
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-10 19:08:14 -0700

    Initial commit. Just boilerplate"

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        {/*
+        - reset
+        - set up Router
+        - set up for StyledComponents
+        - create first Route to HomePage
+        - create Route for LandingPage
+        */}
+      </div>
+    );
+  }
+}
+
+export default App;
